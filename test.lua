f579421e37d13e42ccdcccf7725f7a16516abc96
ZeoHub <romanozchristian0@gmail.com>
2025-06-19 12:03:46 +0800

    Update test.lua

--- test.lua
+++ test.lua
@@ -10,3 +10,15 @@ end
 -- 3. Teleport to the desired place
 local PLACE_ID = 126884695634066 -- Change this to your desired PlaceId
 game:GetService("TeleportService"):Teleport(PLACE_ID)
+
+-- 4. Detect if you are in a private server
+if game.PrivateServerId ~= "" then
+    local placeId = game.PlaceId
+    local code = game.PrivateServerId
+    print("You are in a private server!")
+    print("Private server ID: " .. code)
+    local joinLink = ("https://www.roblox.com/games/%d?privateServerLinkCode=%s"):format(placeId, code)
+    print("Join link: " .. joinLink)
+else
+    print("Not in a private server.")
+end
