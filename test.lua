b2d8b9f68531d0868d788ca876469b0af70ce7d1
ZeoHub <romanozchristian0@gmail.com>
2025-06-19 12:13:08 +0800

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,13 +1,16 @@
+-- === 4. Detect private server status and print info ===
+print("PrivateServerId:", tostring(game.PrivateServerId))
+print("PrivateServerOwnerId:", tostring(game.PrivateServerOwnerId))
+print("JobId:", tostring(game.JobId))
 
-
--- === 3. Detect if you are in a private server ===
 if game.PrivateServerId ~= "" then
     local placeId = game.PlaceId
     local code = game.PrivateServerId
-    print("You are in a private server!")
-    print("Private server ID: " .. code)
+    print("You are in an official private server!")
     local joinLink = ("https://www.roblox.com/games/%d?privateServerLinkCode=%s"):format(placeId, code)
     print("Join link: " .. joinLink)
+elseif game.JobId ~= "" and game.PrivateServerOwnerId == "" then
+    print("You might be in a reserved/matchmaking server (not an official private server)!")
 else
-    print("Not in a private server.")
+    print("You are in a public server.")
 end
